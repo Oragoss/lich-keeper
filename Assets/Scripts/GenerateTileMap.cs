91aa37ead0a9c5e3905fbd2e3db204f0f74fc6ff
Jon Sousa <jonathonsousa@gmail.com>
2023-04-19 13:48:59 -0500

    Added basic tile map example

---
+++ Assets/Scripts/GenerateTileMap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTileMap : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] tile;
+
+    private void Awake()
+    {
+            
+    }
+
+    //TODO: Make a scriptable asset class that contains all of the information about how to set up the tiles for the initial generation of each level
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lich-keeper
+# Lich Keeper
+
+## A dungeon keeper game that focuses on an evil lich trying to build his ultimate tower. If only those pesky do gooders could stay well enough away.
